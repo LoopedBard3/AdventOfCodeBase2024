bcfdbc5a2edf8ea57c35e852a2252d7cc5fbcd9f
Parker Bibus <pbibus@hotmail.com>
2024-12-11 15:41:09 -0800

    Finished Day 10 solution.

--- AdventOfCode.Solutions/Year2024/Day10/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day10/Solution.cs
@@ -1,16 +1,99 @@
+
 namespace AdventOfCode.Solutions.Year2024.Day10;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(10, 2024, "") { }
+    public Solution() : base(10, 2024, "") 
+    {
+        Debug = false;
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var numberGrid = new List<List<int>>();
+        foreach (var line in Input.SplitByNewline())
+        {
+            var lineList = new List<int>();
+            foreach(var number in line)
+            {
+                lineList.Add(number - 48);
+            }
+            numberGrid.Add(lineList);
+        }
+        var width = numberGrid[0].Count;
+        var height = numberGrid.Count;
+
+        var trailheadSum = 0;
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                if (numberGrid[row][col] == 0)
+                {
+                    var trailheads = CheckDirectionsForNextHeight(numberGrid, row, col, numberGrid[row][col]);
+                    trailheadSum += trailheads.Distinct().Count();
+                }
+            }
+        }
+        return trailheadSum.ToString();
+    }
+
+    private List<Tuple<int, int>> CheckDirectionsForNextHeight(List<List<int>> numberGrid, int row, int col, int curHeight)
+    {
+        if(curHeight == 9)
+        {
+            return [new(row, col)];
+        }
+
+        List<Tuple<int, int>> trailheads = [];
+        // Check each of the four cardinal directions for next number and call if valid
+        if (row > 0 && numberGrid[row - 1][col] == curHeight + 1)
+        {
+            trailheads.AddRange(CheckDirectionsForNextHeight(numberGrid, row - 1, col, curHeight + 1));
+        } 
+        if (row < numberGrid.Count - 1 && numberGrid[row + 1][col] == curHeight + 1)
+        {
+            trailheads.AddRange(CheckDirectionsForNextHeight(numberGrid, row + 1, col, curHeight + 1));
+        }
+       if (col > 0 && numberGrid[row][col - 1] == curHeight + 1)
+        {
+            trailheads.AddRange(CheckDirectionsForNextHeight(numberGrid, row, col - 1, curHeight + 1));
+        }
+        if (col < numberGrid[0].Count - 1 && numberGrid[row][col + 1] == curHeight + 1)
+        {
+            trailheads.AddRange(CheckDirectionsForNextHeight(numberGrid, row, col + 1, curHeight + 1));
+        }
+
+        return trailheads;
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var numberGrid = new List<List<int>>();
+        foreach (var line in Input.SplitByNewline())
+        {
+            var lineList = new List<int>();
+            foreach (var number in line)
+            {
+                lineList.Add(number - 48);
+            }
+            numberGrid.Add(lineList);
+        }
+        var width = numberGrid[0].Count;
+        var height = numberGrid.Count;
+
+        var trailheadSum = 0;
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                if (numberGrid[row][col] == 0)
+                {
+                    var trailheads = CheckDirectionsForNextHeight(numberGrid, row, col, numberGrid[row][col]);
+                    trailheadSum += trailheads.Count();
+                }
+            }
+        }
+        return trailheadSum.ToString();
     }
 }
